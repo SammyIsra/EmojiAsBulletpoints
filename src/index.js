9f89d808943453afff20b0606006e93e2f67138e
Sammy Israwi <sammy.israwi@gmail.com>
2018-04-11 19:45:56 -0400

    Default Emoji-As-List has been developed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react'
+import PropTypes from "prop-types";
 
-export default class extends Component {
+class DefaultEmojiList extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const {itemList, emojis} = this.props;
+    const emojiListLength = emojis.length;
+    console.log(this.props);
+    return (
+      <ul style={{listStyleType: "none"}}>
+        {itemList.map((listItem, listItemIndex)=>{
+          const emojiLoc = listItemIndex % emojiListLength;
+          return <li>{emojis[emojiLoc]} {listItem}</li>;
+        })}
+      </ul>
+    );
   }
 }
+
+DefaultEmojiList.propTypes = {
+  emojis: PropTypes.arrayOf(PropTypes.string).isRequired,
+  itemList: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+
+export default DefaultEmojiList;
