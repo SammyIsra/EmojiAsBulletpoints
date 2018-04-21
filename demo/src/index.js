ccfb56b6955b541dfaee1ec825fb8ff8d03536e9
Sammy Israwi <sammy.israwi@gmail.com>
2018-04-21 17:37:27 -0400

    Added a demo for the render props component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import EmojiList from '../../src';
+import DefaultEmojiList, {EmojiRenderProps} from '../../src';
 
 const emojis = ["🔥", "✔️", "🐔"];
 const list = [
@@ -9,15 +9,32 @@ const list = [
   "Correct?",
   "Dinner",
   "BURN!",
-  "Green Vee"
+  "Green Vee",
+  "I lost count",
+  "Is this the chicken one again?",
+  "...oh"
 ]
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>emoji-as-bulletpoints Demo</h1>
-      <EmojiList emojis={emojis} itemList={list} />
-    </div>
+    return (
+      <div>
+        <h1>emoji-as-bulletpoints Demo</h1>
+        <h2>Default Emoji List:</h2>
+        <DefaultEmojiList emojis={emojis} itemList={list} />
+        <h2>Render Props Emoji List:</h2>
+        <EmojiRenderProps emojis={emojis} itemList={list}>
+        { (wholeList) => (
+          <ul style={{listStyleType: "none"}}>
+          {wholeList.map(({emoji, listItem}) => (
+            <li>{emoji} {listItem}</li>
+          ))}
+          </ul>
+          //<p>{wholeList[0].emoji} {wholeList[0].listItem}</p>
+        )}</EmojiRenderProps>
+        
+      </div>
+    );
   }
 }
 
